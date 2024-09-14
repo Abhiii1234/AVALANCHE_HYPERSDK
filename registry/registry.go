f465b3bbce22ff41be43d01b389f88258c6f9cff
Abhigyan Pushkar <149153539+Abhiii1234@users.noreply.github.com>
2024-09-14 13:00:31 +0530

    Update registry.go

--- registry/registry.go
+++ registry/registry.go
@@ -1,48 +1,40 @@
 // Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-package registry
+package consts
 
 import (
-	"github.com/ava-labs/avalanchego/utils/wrappers"
+	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/vms/platformvm/warp"
 	"github.com/ava-labs/hypersdk/chain"
 	"github.com/ava-labs/hypersdk/codec"
-
-	"tokenvm/actions"
-	"tokenvm/auth"
-	"tokenvm/consts"
+	"github.com/ava-labs/hypersdk/consts"
 )
 
-// Setup types
+const (
+	// TODO: choose a human-readable part for your hyperchain
+	HRP = "Crick VM"
+	// TODO: choose a name for your hyperchain
+	Name = "Crick"
+	// TODO: choose a token symbol
+	Symbol = "CK"
+)
+
+var ID ids.ID
+
 func init() {
-	consts.ActionRegistry = codec.NewTypeParser[chain.Action, *warp.Message]()
-	consts.AuthRegistry = codec.NewTypeParser[chain.Auth, *warp.Message]()
-
-	errs := &wrappers.Errs{}
-	errs.Add(
-		// When registering new actions, ALWAYS make sure to append at the end.
-		consts.ActionRegistry.Register(&actions.Transfer{}, actions.UnmarshalTransfer, false),
-
-		// TODO: register action: actions.CreateAsset
-		consts.ActionRegistry.Register(&actions.UnmarshalCreateAsset{}, actions.UnmarshalCreateAsset, false),
-		// TODO: register action: actions.MintAsset
-		consts.ActionRegistry.Register(&actions.UnmarshalMintAsset{}, actions.UnmarshalMintAsset, false),
-
-		consts.ActionRegistry.Register(&actions.BurnAsset{}, actions.UnmarshalBurnAsset, false),
-		consts.ActionRegistry.Register(&actions.ModifyAsset{}, actions.UnmarshalModifyAsset, false),
-
-		consts.ActionRegistry.Register(&actions.CreateOrder{}, actions.UnmarshalCreateOrder, false),
-		consts.ActionRegistry.Register(&actions.FillOrder{}, actions.UnmarshalFillOrder, false),
-		consts.ActionRegistry.Register(&actions.CloseOrder{}, actions.UnmarshalCloseOrder, false),
-
-		consts.ActionRegistry.Register(&actions.ImportAsset{}, actions.UnmarshalImportAsset, true),
-		consts.ActionRegistry.Register(&actions.ExportAsset{}, actions.UnmarshalExportAsset, false),
-
-		// When registering new auth, ALWAYS make sure to append at the end.
-		consts.AuthRegistry.Register(&auth.ED25519{}, auth.UnmarshalED25519, false),
-	)
-	if errs.Errored() {
-		panic(errs.Err)
+	b := make([]byte, consts.IDLen)
+	copy(b, []byte(Name))
+	vmID, err := ids.ToID(b)
+	if err != nil {
+		panic(err)
 	}
+	ID = vmID
 }
+
+// Instantiate registry here so it can be imported by any package. We set these
+// values in [controller/registry].
+var (
+	ActionRegistry *codec.TypeParser[chain.Action, *warp.Message, bool]
+	AuthRegistry   *codec.TypeParser[chain.Auth, *warp.Message, bool]
+)
