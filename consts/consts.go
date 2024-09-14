69adec04978f222e6328973492896ab1e8805b4b
Abhigyan Pushkar <149153539+Abhiii1234@users.noreply.github.com>
2024-09-14 13:02:53 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = "ABHI VM"
+	HRP = "Crick VM"
 	// TODO: choose a name for your hyperchain
-	Name = "School"
+	Name = "Crick"
 	// TODO: choose a token symbol
-	Symbol = "AIM"
+	Symbol = "CK"
 )
 
 var ID ids.ID
